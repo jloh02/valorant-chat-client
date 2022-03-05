9461dea1e31ecd7ccfbd046c99bdb75ffffac99c
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-03-05 19:59:04 +0800

    Add loading circle

--- src/views/RiotClientClosed.vue
+++ src/views/RiotClientClosed.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="riot-client-closed">
-    <div>Error</div>
+    <div class="loader">
+      <svg class="circular" viewBox="25 25 50 50">
+        <circle
+          class="path"
+          cx="50"
+          cy="50"
+          r="20"
+          fill="none"
+          stroke-width="2"
+          stroke-miterlimit="10"
+        />
+      </svg>
+    </div>
+    <p>
+      Riot Client Closed<br />
+      Waiting for Response...
+    </p>
   </div>
 </template>
 
@@ -13,7 +29,83 @@ export default defineComponent({
 </script>
 
 <style lang="postcss">
+/* Taken from https://www.cssscript.com/animated-google-loader-with-svg-and-css/ */
+
 .riot-client-closed {
   @apply flex flex-col justify-center items-center;
+}
+.loader {
+  position: relative;
+  margin: 12px auto;
+  width: 48px;
+}
+.loader:before {
+  content: "";
+  display: block;
+  padding-top: 100%;
+}
+.circular {
+  -webkit-animation: rotate 2s linear infinite;
+  animation: rotate 2s linear infinite;
+  height: 100%;
+  -webkit-transform-origin: center center;
+  -ms-transform-origin: center center;
+  transform-origin: center center;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  @apply stroke-red-600;
+}
+.path {
+  stroke-dasharray: 1, 200;
+  stroke-dashoffset: 0;
+  -webkit-animation: dash 1.5s ease-in-out infinite,
+    color 6s ease-in-out infinite;
+  animation: dash 1.5s ease-in-out infinite, color 6s ease-in-out infinite;
+  stroke-linecap: round;
+}
+@-webkit-keyframes rotate {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotate {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes dash {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -35;
+  }
+  100% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -124;
+  }
+}
+@keyframes dash {
+  0% {
+    stroke-dasharray: 1, 200;
+    stroke-dashoffset: 0;
+  }
+  50% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -35;
+  }
+  100% {
+    stroke-dasharray: 89, 200;
+    stroke-dashoffset: -124;
+  }
 }
 </style>
