bf75c7aa232a2300568a7eae44da7f1cd6c5434c
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-03-13 20:33:33 +0800

    Changes window pkg to regedit pkg

--- src/main/windows_util.ts
+++ src/main/windows_util.ts
@@ -1,11 +1,25 @@
-import { registry } from "windows";
+import path from "path";
+import log from "electron-log";
 import child_process from "child_process";
 
 export async function runRiotClient() {
-  const reg = registry(
-    "HKCU/SOFTWARE/Microsoft/Windows/CurrentVersion/Uninstall/Riot Game valorant.live"
+  let regedit = require("regedit");
+  regedit.setExternalVBSLocation(path.join(__static, "vbs"));
+
+  const regPath =
+    "HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Riot Game valorant.live";
+  regedit.list(
+    regPath,
+    function (
+      err: Error | undefined,
+      result: RegistryItemCollection<string[]>
+    ) {
+      if (err) log.warn(err);
+      if (result[regPath].exists) {
+        const unins = result[regPath].values.UninstallString.value;
+        log.debug(unins);
+        if (unins) child_process.exec(unins.split(" --uninstall")[0]);
+      }
+    }
   );
-  const unins = reg.UninstallString.value;
-  if (unins)
-    child_process.exec(reg.UninstallString.value.split(" --uninstall")[0]);
 }
