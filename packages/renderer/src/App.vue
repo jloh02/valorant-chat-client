71438fa3cc68d0d6576470a995adb066a0babb46
jloh02 <36648707+jloh02@users.noreply.github.com>
2022-05-16 18:33:31 +0800

    Add CSS pre and post processors

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,25 +1,25 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <div class="logo-box">
-    <img style="height:140px;" src="./assets/electron.png" >
-    <span/>
-    <img style="height:140px;" src="./assets/vite.svg" >
-    <span/>
-    <img style="height:140px;" src="./assets/vue.png" >
+    <img style="height: 140px" src="./assets/electron.png" />
+    <span />
+    <img style="height: 140px" src="./assets/vite.svg" />
+    <span />
+    <img style="height: 140px" src="./assets/vue.png" />
   </div>
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   <div class="static-public">
     Place static files into the <code>src/renderer/public</code> folder
-    <img style="width:90px;" :src="'./images/node.png'" >
+    <img style="width: 90px" :src="'./images/node.png'" />
   </div>
 </template>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -33,23 +33,23 @@ import HelloWorld from './components/HelloWorld.vue'
   display: flex;
   width: 100%;
   justify-content: center;
-}
 
-.logo-box span {
-  width: 74px;
+  span {
+    width: 74px;
+  }
 }
 
 .static-public {
   display: flex;
   align-items: center;
   justify-content: center;
-}
 
-.static-public code {
-  background-color: #eee;
-  padding: 2px 4px;
-  margin: 0 4px;
-  border-radius: 4px;
-  color: #304455;
+  code {
+    background-color: #eee;
+    padding: 2px 4px;
+    margin: 0 4px;
+    border-radius: 4px;
+    color: #304455;
+  }
 }
 </style>
